2eb9e751cf58cb3201806ee1e9f24a53cc6ca688
charles <charleshparmley@icloud.com>
2022-02-09 19:40:38 -0600

    initial commit

--- VideoPlayer/ContentView.swift
+++ VideoPlayer/ContentView.swift
@@ -4,13 +4,22 @@
 //
 //  Created by Charles Parmley on 2/9/22.
 //
-
+import AVKit
 import SwiftUI
 
 struct ContentView: View {
+    let url = URL(string: "https://www.pexels.com/video/3831869/download/")!
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack{
+                VideoPlayer(player: AVPlayer(url:url),
+                    videoOverlay: {
+                        Text("Charles Video Test").frame(width: 200, height: 20).background(Color.blue)
+                }).scaledToFit()
+            }
+        }
     }
 }
 
